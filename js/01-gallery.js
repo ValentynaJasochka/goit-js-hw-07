b595aff0959228d24320473a5d54c652b9e059d2
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2023-06-22 21:01:00 +0300

    first attempt

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+function createMarkupList(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        ` <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+    )
+    .join(" ");
+}
+list.insertAdjacentHTML("afterbegin", createMarkupList(galleryItems));
+
+list.addEventListener("click", clickImage);
+
+function clickImage(evt) {
+  evt.preventDefault();
+  const cardItem = evt.target.closest(".gallery__item");
+  if (!cardItem) {
+    return;
+  } else {
+    function presEsc(event) {
+      if (event.code === "Escape") {
+        instance.close();
+      }
+    }
+    const item = evt.target.dataset.source;
+    const instance = basicLightbox.create(createModal(item), {
+      onShow: () => window.addEventListener("keydown", presEsc),
+      onClose: () => window.removeEventListener("keydown", presEsc),
+    });
+
+    instance.show();
+  }
+}
+
+function createModal(link) {
+  return `
+    <img src="${link}" width="800" height="600">
+`;
+}
